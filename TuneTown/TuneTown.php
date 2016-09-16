76039a2debf323dda70e43427a77d10cf46929e1
jmartin5076 <j-martin.3@onu.edu>
2016-09-16 13:16:14 -0400

    Add files via upload

---
+++ TuneTown/TuneTown.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package TuneTown
+ */
+/*
+Plugin Name: TuneTown
+Plugin URI: https://wingsauce.net/tunetown
+Description: Will install a preset list of instrumental musical pieces that start playing when the site is open and active providing a refreshing ambiance while you browse and explore.
+Version: 1.0
+Author: Jeremy Martin and Corey McLaughlin
+Author URI: https://wingsauce.net/author
+License: GPLv2 or later
+License URI: https://wordpress.org/about/gpl/
+*/
